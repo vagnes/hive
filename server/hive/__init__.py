0504bf5d8827bb376bafcf87358586f58924feba
vagnes <waagnes@outlook.com>
2019-06-24 13:06:53 +0200

    Added CORS

--- server/hive/__init__.py
+++ server/hive/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "thisisatestkeyfortheloveofgodchangeit"
@@ -8,5 +9,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 socketio = SocketIO(app)
 db = SQLAlchemy(app)
+CORS(app)
 
 from hive import routes

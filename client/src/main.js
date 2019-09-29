de4865192dbe55c3b42564a039a83e97d7d7392e
Mathias B. Vagnes <vagnes@protonmail.com>
2019-09-29 22:59:20 +0200

    temporary fix because dotenv is shit

--- client/src/main.js
+++ client/src/main.js
@@ -1,21 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueSocketIO from 'vue-socket.io';
-import 'normalize.css';
-import router from './router'
-import Ionic from '@ionic/vue';
-import '@ionic/core/css/ionic.bundle.css';
+import Vue from "vue";
+import App from "./App.vue";
+import VueSocketIO from "vue-socket.io";
+import "normalize.css";
+import router from "./router";
+import Ionic from "@ionic/vue";
+import "@ionic/core/css/ionic.bundle.css";
 
 Vue.use(Ionic);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(new VueSocketIO({
-  debug: true,
-  connection: "localhost:5000"
-}))
-
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: "192.168.0.101:5000"
+  })
+);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
